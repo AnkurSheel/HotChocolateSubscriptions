22fa4555cba4562100158396ded8edbc55a2e0e7
Ankur Sheel <ankur.sheel@gmail.com>
2021-10-07 11:34:12 +1300

    use redis

--- GraphqlSubscriptions/Startup.cs
+++ GraphqlSubscriptions/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
 
 namespace GraphqlSubscriptions
 {
@@ -11,7 +12,11 @@ namespace GraphqlSubscriptions
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGraphQLServer().AddQueryType<Query>().AddMutationType<Mutation>().AddSubscriptionType<Subscription>().AddInMemorySubscriptions();
+            services.AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddMutationType<Mutation>()
+                .AddSubscriptionType<Subscription>()
+                .AddRedisSubscriptions(_ => ConnectionMultiplexer.Connect("localhost:6379"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
